ad3053d02ab6825937c33d122c6704fa76109e4a
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2022-08-10 20:48:10 +0200

    mech work put on hold

--- content/Mech/Mech.cs
+++ content/Mech/Mech.cs
@@ -6,7 +6,7 @@ public static partial class Mech
     [IComponent.Data(Net.SendType.Reliable)]
     public partial struct Data : IComponent
     {
-        public float leg_reach = 5;
+        public float leg_reach = 10;
         public float angle = 45;
         public float max_angle = 85;
         public float max_normal = 140;
@@ -22,6 +22,9 @@ public static partial class Mech
     [IComponent.Data(Net.SendType.Unreliable)]
     public partial struct State : IComponent
     {
+        public FixedArray8<EntRef<MechLeg.Data>> legs = new FixedArray8<EntRef<MechLeg.Data>>();
+        public int currentLeg = 0;
+        public int numLegs = -1;
 
         public State()
         {
@@ -29,19 +32,18 @@ public static partial class Mech
         }
     }
 
-
-
     [ISystem.Update(ISystem.Mode.Single)]
     public static void Update(Entity ent_mech, ISystem.Info info, [Source.Owned] ref Control.Data control, [Source.Owned] ref Vehicle.Data vehicle, 
         [Source.Owned] ref Transform.Data transform, [Source.Owned] ref Mech.Data mech, [Source.Owned] ref Mech.State state)
     {
+        bool move = false;
         if (control.keyboard.GetKey(Keyboard.Key.MoveRight))
         {
             mech.angle = 45;
             mech.max_angle = 85;
             mech.max_normal = 45;
             mech.increment = 5;
-
+            move = true;
         }
         else if (control.keyboard.GetKey(Keyboard.Key.MoveLeft))
         {
@@ -49,6 +51,7 @@ public static partial class Mech
             mech.max_angle = Maths.OppositeAngle(85 * Maths.deg2rad) * Maths.rad2deg;
             mech.max_normal = 45;
             mech.increment = -5;
+            move = true;
         }
         else
         {
@@ -64,7 +67,7 @@ public static partial class Mech
                 break;
             }
 
-            hit = info.GetRegion().TryLinecast(transform.position, transform.position + MathN.VectorByAngleDeg(angle, 10), 0, out LinecastResult result, mask: Physics.Layer.World, query_flags: Physics.QueryFlag.Static);
+            hit = info.GetRegion().TryLinecast(transform.position, transform.position + MathN.VectorByAngleDeg(angle, mech.leg_reach), 0, out LinecastResult result, mask: Physics.Layer.World, query_flags: Physics.QueryFlag.Static);
 
             if (hit && result.normal.GetAngleDegrees() > -90 - mech.max_normal && result.normal.GetAngleDegrees() < -90 + mech.max_normal)
             {
@@ -77,24 +80,22 @@ public static partial class Mech
 
             angle += mech.increment;
         } while (!hit);
+    NoInput:
 
-        NoInput:
-
-        Span<Entity> children = stackalloc Entity[8];
-        ent_mech.GetChildren(ref children, Relation.Type.Instance);
-
-        foreach (var ent_attachment in children)
+        ref MechLeg.Data leg =  ref state.legs[state.currentLeg].GetValueOrNullRef();
+        if (!leg.IsNull())
         {
-            ref var attachment = ref ent_attachment.GetComponent<Attachment.Data>();
-            if (!attachment.IsNull())
-            {
-                if (ent_attachment.TryGetChild(Relation.Type.Child, out Entity ent_leg))
-                {
-                    ref var leg = ref ent_leg.GetComponent<MechLeg.Data>();
-                    if (!leg.IsNull())
-                    {
 
-                    }
+
+            leg.destination = mech.targetPos;
+
+
+            if (leg.atDestination)
+            {
+                state.currentLeg++;
+                if (state.currentLeg > state.numLegs)
+                {
+                    state.currentLeg = 0;
                 }
             }
         }
@@ -123,11 +124,11 @@ public static partial class Mech
                         break;
                     }
 
-                    hit = Client.GetRegion().TryLinecast(transform.position, transform.position + MathN.VectorByAngleDeg(angle, 10), 0, out LinecastResult result, mask: Physics.Layer.World, query_flags: Physics.QueryFlag.Static);
+                    hit = Client.GetRegion().TryLinecast(transform.position, transform.position + MathN.VectorByAngleDeg(angle, data.leg_reach), 0, out LinecastResult result, mask: Physics.Layer.World, query_flags: Physics.QueryFlag.Static);
                     
                     if (hit && result.normal.GetAngleDegrees() > -90 -data.max_normal && result.normal.GetAngleDegrees() < -90 + data.max_normal)
                     {
-                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, 10)), Color32BGRA.Green, 1);
+                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, data.leg_reach)), Color32BGRA.Green, 1);
                         GUI.DrawCircle(GUI.WorldToCanvas(result.world_position), 5, Color32BGRA.Green);
                         GUI.DrawLine(GUI.WorldToCanvas(result.world_position), GUI.WorldToCanvas(result.world_position + result.normal), new Color32BGRA(255, 0, 0, 255), 1);
 
@@ -136,7 +137,7 @@ public static partial class Mech
                     }
                     else if (hit)
                     {
-                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, 10)), Color32BGRA.Yellow, 1);
+                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, data.leg_reach)), Color32BGRA.Yellow, 1);
                         GUI.DrawCircle(GUI.WorldToCanvas(result.world_position), 5, Color32BGRA.Yellow);
                         GUI.DrawLine(GUI.WorldToCanvas(result.world_position), GUI.WorldToCanvas(result.world_position + result.normal), new Color32BGRA(255, 0, 0, 255), 1);
 
@@ -145,8 +146,8 @@ public static partial class Mech
                     }
                     else
                     {
-                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, 10)), Color32BGRA.Red, 1);
-                        GUI.DrawCircle(GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, 10)), 5, Color32BGRA.Red);
+                        GUI.DrawLine(GUI.WorldToCanvas(transform.position), GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, data.leg_reach)), Color32BGRA.Red, 1);
+                        GUI.DrawCircle(GUI.WorldToCanvas(transform.position + MathN.VectorByAngleDeg(angle, data.leg_reach)), 5, Color32BGRA.Red);
                     }
 
                     angle += data.increment;
--- content/Mech/MechLeg.cs
+++ content/Mech/MechLeg.cs
@@ -7,19 +7,77 @@ public static partial class MechLeg
     [IComponent.Data(Net.SendType.Reliable)]
     public partial struct Data : IComponent
     {
-        
+        public float length_a = 4;
+        public float length_b = 4;
+        public Vector2 destination = Vector2.Zero;
+        public bool move = true;
+        public bool atDestination = false;
+        public float precision = 0.5f;
+
         public Data()
         {
 
         }
     }
 
+    [IComponent.Data(Net.SendType.Unreliable)]
+    public partial struct State : IComponent
+    {
+        public float angle_a = 0;
+        public float angle_b = 0;
+
+        public State()
+        {
+
+        }
+    }
+
+    [ISystem.Add(ISystem.Mode.Single)]
+    public static void Add(Entity ent_leg, [Source.Parent] ref Attachment.Data slot, [Source.Parent] ref Mech.Data mech, [Source.Parent] ref Mech.State mechState,
+        [Source.Owned] ref MechLeg.Data leg)
+    {
+        for (int i = 0; i < mechState.legs.Length; i++)
+        {
+            if (mechState.legs[i].entity == Entity.None)
+            {
+                mechState.legs[i].Set(ent_leg);
+                mechState.numLegs ++;
+                break;
+            }
+        }
+    }
+
+    [ISystem.Remove(ISystem.Mode.Single)]
+    public static void Remove(Entity ent_leg, [Source.Parent] ref Attachment.Data slot, [Source.Parent] ref Mech.Data mech, [Source.Parent] ref Mech.State mechState,
+    [Source.Owned] ref MechLeg.Data leg)
+    {
+        for (int i = 0; i < mechState.legs.Length; i++)
+        {
+            if (mechState.legs[i].entity == ent_leg)
+            {
+                mechState.legs[i].Set(Entity.None);
+                mechState.numLegs--;
+                break;
+            }
+        }
+    }
+
 
 
     [ISystem.Update(ISystem.Mode.Single)]
-    public static void Update(Entity ent_joint, [Source.Owned] ref Joint.Base joint, [Source.Owned, Original] ref Joint.Gear gear, [Source.Owned] ref Joint.Pivot pivot,
-        [Source.Parent] ref MechLeg.Data data, [Source.Parent, Original] ref Joint.Gear cabGear)
+    public static void Update(Entity ent_joint, [Source.Owned] ref Joint.Base joint, [Source.Owned, Original] ref Joint.Gear legGear, [Source.Owned] ref Joint.Pivot pivot,
+        [Source.Owned] ref MechLeg.Data leg, [Source.Parent, Original] ref Joint.Gear cabGear, [Source.Parent] ref Mech.Data mech, [Source.Owned] ref Transform.Data transform,
+        [Source.Owned] ref MechLeg.State state, [Source.Parent] ref Joint.Base parentJoint)
     {
-        
+        if (leg.move)
+        {
+            IK.Resolve2x(new Vector2(leg.length_a, leg.length_b), transform.LocalToWorld(parentJoint.offset_b), leg.destination, new Vector2(state.angle_a, state.angle_b), out Vector2 newAngles, false);
+            state.angle_a = newAngles.X;
+            state.angle_b = newAngles.Y;
+
+            cabGear.rotation = state.angle_a;
+            legGear.rotation = state.angle_b;
+
+        }
     }
 }
